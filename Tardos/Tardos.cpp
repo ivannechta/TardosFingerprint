9c34a72dd0a96931fed993ba1d4f70553147f03a
ivandows <tal200@lenta.ru>
2020-07-26 22:11:40 +0700

    1

--- Tardos/Tardos.cpp
+++ Tardos/Tardos.cpp
@@ -2,10 +2,12 @@
 //
 
 #include <iostream>
+#include "Tardos.h"
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
